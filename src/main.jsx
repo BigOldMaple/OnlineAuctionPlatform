80f69213b21f33cc98c1fc4e0a3f1b35c9124470
anan1111 <andylealwow@gmail.com>
2024-11-07 15:53:45 +0000

    OAuth integration + DaisyUI navbar
    You'll have to create a .env file in the root folder with your own OAuth domain and client ID for it to work.

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import auctionReducer from "./features/auctionSlice";
-import './index.css';
-
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { ToastContainer } from "react-toastify";
 
 const store = configureStore({
   reducer: {
@@ -13,8 +14,24 @@ const store = configureStore({
   },
 });
 
+const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
+const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+console.log(AUTH0_CLIENT_ID);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <>
+    <Auth0Provider
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <Provider store={store}>
+        <ToastContainer position="top-center" />
+        <App />
+      </Provider>
+    </Auth0Provider>
+  </>
 );
